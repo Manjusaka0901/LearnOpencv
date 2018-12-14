e20176a107ddf4265a37c4f3f55f786c7c2ca957
Manjusaka0901 <huangcaili0901@163.com>
2018-12-14 13:29:08 +0800

    init files

---
+++ opencvLearn/Source.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<opencv2\core\core.hpp>
+#include<opencv2\highgui\highgui.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char * argv[])
+{
+	Mat img;
+
+	imshow
+
+	return 0;
+}
